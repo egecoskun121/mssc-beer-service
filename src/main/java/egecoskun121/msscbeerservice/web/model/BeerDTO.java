7c26ca05f8d0bfcc59edf07a0da6150d3aabdd77
egecoskun121 <77891949+egecoskun121@users.noreply.github.com>
2022-10-14 12:02:17 +0300

    Added jackson annotations

--- src/main/java/egecoskun121/msscbeerservice/web/model/BeerDTO.java
+++ src/main/java/egecoskun121/msscbeerservice/web/model/BeerDTO.java
@@ -1,15 +1,13 @@
 package egecoskun121.msscbeerservice.web.model;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
-import javax.validation.constraints.Positive;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -28,12 +26,15 @@ public class BeerDTO {
     private Integer version;
 
     @Null
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
 
     @Null
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
 
     @NotBlank
+    @Size(min=3,max=100)
     private String beerName;
 
     @NotNull
@@ -43,11 +44,12 @@ public class BeerDTO {
     @Positive
     private Long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
 
-
+    @Positive
     private Integer quantityOnHand;
 
 }
