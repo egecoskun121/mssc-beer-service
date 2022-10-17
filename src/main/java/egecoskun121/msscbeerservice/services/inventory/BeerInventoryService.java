83024e6ac760b975f47b34eeb6884b6d388d629b
egecoskun121 <77891949+egecoskun121@users.noreply.github.com>
2022-10-17 15:55:15 +0300

    Implemented BeerInventoryService

---
+++ src/main/java/egecoskun121/msscbeerservice/services/inventory/BeerInventoryService.java
@@ -0,0 +1,7 @@
+package egecoskun121.msscbeerservice.services.inventory;
+
+import java.util.UUID;
+
+public interface BeerInventoryService {
+    Integer getOnHandInventory(UUID beerId);
+}
---
+++ src/test/java/egecoskun121/msscbeerservice/inventory/services/BeerInventoryServiceRestTemplateImplTest.java
@@ -0,0 +1,28 @@
+package egecoskun121.msscbeerservice.inventory.services;
+
+import egecoskun121.msscbeerservice.bootstrap.BeerLoader;
+import egecoskun121.msscbeerservice.services.inventory.BeerInventoryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest
+public class BeerInventoryServiceRestTemplateImplTest {
+
+    @Autowired
+    BeerInventoryService beerInventoryService;
+
+    @BeforeEach
+    void setUp(){
+
+    }
+
+    @Test
+    void getOnHandInventory(){
+        Integer qoh = beerInventoryService.getOnHandInventory(BeerLoader.BEER_1_UUID);
+    }
+
+}
